ee0ddb3d02117cae2c0cf806ea9d57756f67fc21
nprindle <nprindle@gatech.edu>
2019-04-06 23:00:32 -0400

    Automate creation of manifest files

---
+++ prefer_external_manifest/setup_manifests.sh
@@ -0,0 +1,10 @@
+cat manifests.txt | tr -d '\r' |
+while read -r line; do
+    if [[ $line =~ ^(.+/)* ]]; then
+        cp program.manifest "${BASH_REMATCH[0]}" &&
+        mv "${BASH_REMATCH[0]}/program.manifest" "$line.manifest" &&
+        echo Created $line.manifest
+    else
+        echo "Error: $line"
+    fi
+done
