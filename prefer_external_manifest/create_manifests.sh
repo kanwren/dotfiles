4d8eca9d3a6c3c8e80775dcd0244c8860f48b1be
nprindle <nprindle@gatech.edu>
2019-04-26 23:45:13 -0400

    Fix line reading

--- prefer_external_manifest/create_manifests.sh
+++ prefer_external_manifest/create_manifests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cat manifests.txt | tr -d '\r' |
-while read -r line; do
+while IFS='' read -r line || [[ -n "$line" ]]; do
     if [[ $line =~ ^(.+/)* ]]; then
         cp program.manifest "${BASH_REMATCH[0]}" &&
         mv "${BASH_REMATCH[0]}/program.manifest" "$line.manifest" &&
