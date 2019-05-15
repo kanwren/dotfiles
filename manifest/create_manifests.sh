a32c723a93673441543ff41af6aabb8720573d90
nprindle <nprindle18@gmail.com>
2019-05-15 16:54:20 -0400

    Add functionality for backslashes and comment lines

--- manifest/create_manifests.sh
+++ manifest/create_manifests.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-cat manifests.txt | tr -d '\r' |
+
+# Strip comment lines
+grep -Ev '^\s*#' manifests.txt |
+# Strip Windows endlines
+tr -d '\r' |
+# Fix Windows backslashes in paths
+tr -d '\\' '/' |
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    if [[ $line =~ ^(.+/)* ]]; then
-        cp program.manifest "${BASH_REMATCH[0]}" &&
-        mv "${BASH_REMATCH[0]}/program.manifest" "$line.manifest" &&
-        echo Created $line.manifest
+    if [[ $line =~ ^.+\.exe ]]; then
+        cp -v program.manifest "$line.manifest" &&
+        echo "Created $line.manifest"
     else
-        echo "Error: $line"
+        echo "Error: invalid .exe file: $line"
     fi
 done
